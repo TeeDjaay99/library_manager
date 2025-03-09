c617a309369515eecf7571dc44696fffb84a4353
TeeDjaay99 <tonyjokiranta@gmail.com>
2025-03-09 14:00:08 +0100

    added some packages
    packages added

---
+++ src/main/java/org/example/mysql/MysqlConnector.java
@@ -0,0 +1,21 @@
+package org.example.mysql;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MysqlConnector {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/library_db";
+    private static final String USER = "root";
+    private static final String PASSWORD = "root";
+
+    public static Connection getConnection() throws SQLException {
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            System.err.println("Error when connecting to database: " + e.getMessage());
+            throw e;
+        }
+    }
+}
