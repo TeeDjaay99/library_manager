7cae05d8a21d3d6e827857462b4a5ead9818291f
TeeDjaay99 <tonyjokiranta@gmail.com>
2025-03-10 18:27:18 +0100

    debugg färdigt

--- src/main/java/org/example/debug/DebugDynamoDb.java
+++ src/main/java/org/example/debug/DebugDynamoDb.java
@@ -1,4 +1,23 @@
 package org.example.debug;
 
+import org.example.data.DynamoDbManager;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+
+
 public class DebugDynamoDb {
+    public static void main(String[] args) {
+        System.out.println("Testing DynamoDB connection...");
+        testDynamoDbConnection();
+    }
+
+    public static void testDynamoDbConnection() {
+
+        try {
+            DynamoDbClient dynamoDb = DynamoDbManager.connect();
+
+            System.out.println("DynamoDb Connection Successful!");
+        } catch (Exception e) {
+            System.err.println("DynamoDb Connection Failed!: " + e.getMessage());
+        }
+    }
 }
--- src/main/java/org/example/debug/DebugMysql.java
+++ src/main/java/org/example/debug/DebugMysql.java
@@ -1,4 +1,40 @@
 package org.example.debug;
 
+import org.example.data.mysql.MysqlConnector;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class DebugMysql {
+
+    public static void main(String[] args) {
+        System.out.println("Testing MySQL connection...");
+        testMysqlConnection();
+    }
+
+    public static void testMysqlConnection() {
+        try (Connection conn = MysqlConnector.getConnection()) {
+            if (conn != null) {
+                System.out.println("MySQL-connection success!");
+
+                String sql = "SELECT book_id, book_title, author FROM books";
+                try (Statement stmt = conn.createStatement();
+                     ResultSet rs = stmt.executeQuery(sql)) {
+
+                    System.out.println("books in the database:");
+                    while (rs.next()) {
+                        System.out.println("Book-ID: " + rs.getInt("book_id") +
+                                " | Title: " + rs.getString("book_title") +
+                                " | Author: " + rs.getString("author"));
+                    }
+                }
+            } else {
+                System.out.println("MySQL-connection failed!");
+            }
+        } catch (SQLException e) {
+            System.err.println("MySql-error: " + e.getMessage());
+        }
+    }
 }
